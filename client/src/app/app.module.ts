24a38fcc3fc0b900e389001c52a0dad04f684fd9
huubv <huubv@kaopiz.com>
2017-11-02 13:56:28 +0700

    add - template admin

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { CoreModule } from './core/core.module'; 
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
