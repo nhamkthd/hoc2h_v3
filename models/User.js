809768b5402bb6b5ffc5487bd1ea2900768c68a5
nhamkthd <nhamkthd92@gmail.com>
2017-11-02 23:47:37 +0700

    update module user and add home componet
    update module user and add home componet

--- models/User.js
+++ models/User.js
@@ -3,8 +3,25 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 const Schema = {
-  username: String,
-  password: String
+  user_name: String,
+  email:String,
+  password: String,
+  state:Number,
+  provider_id:Number,
+  provider_name:String,
+  code:String,
+  profile: {
+  	full_name:String,
+  	job:String,
+  	local:String,
+  	phone:Number,
+  	birthday:Date,
+  	avatar:String,
+  	coins:Number,
+  	reputations:Number,
+  	introduction:String
+  }
+
 }
 
 module.exports = mongoose.model('User', Schema);
