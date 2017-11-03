84d9d1bfaa160650cb2cdda36c625c2dd39ef78e
huubv <huubv@kaopiz.com>
2017-11-03 14:14:35 +0700

    update-auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,43 +1,60 @@
-const jwt  = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 module.exports = function (router) {
 
   // register
-  router.post('/register', function (req, res, next) {
+  router.post('/auth/register', function (req, res, next) {
 
     req.check('username', 'username khong duoc de trong').notEmpty();
     req.check('password', 'password khong duoc de trong').notEmpty();
     req.check('passwordConfirmation', 'passwordConfirmation khong duoc de trong').notEmpty();
-    req.check('password', 'password khong trung khop').custom(value => {return value == req.body.passwordConfirmation});
+    req.check('password', 'password khong trung khop').custom(value => {
+      return value == req.body.passwordConfirmation;
+    });
 
-    User.findOne({username: req.body.username}, function (err, user) {
+    User.findOne({
+      username: req.body.username
+    }, function (err, user) {
 
-      if (user) req.check('username', 'username da ton tai').custom(value => {return false});
+      if (user) req.check('username', 'username da ton tai').custom(value => {
+        return false
+      });
       let errors = req.validationErrors();
       if (errors) return res.status(422).json(errors);
       return next();
-      
+
     });
 
   }, function (req, res) {
 
-      let user = new User();
-      user.username = req.body.username;
-      user.password = req.body.password;
-      user.save(function (err, user) {
-        if (user) {
-          let token = jwt.sign({user: user}, process.env.JWT_SECRET_KEY, {expiresIn: 60*60});
-          return res.status(200).json({token: token});
-        } else {
-          return res.status(401).json([{msg: 'dang ky that bai'}]);
-        }
-      });
+    let user = new User();
+    user.username = req.body.username;
+    user.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+    user.role = 2;
+    user.save(function (err, user) {
+      if (user) {
+        let token = jwt.sign({
+          user: user
+        }, process.env.JWT_SECRET_KEY, {
+          expiresIn: 60 * 60
+        });
+        return res.status(200).json({
+          token: token,
+          user: user
+        });
+      } else {
+        return res.status(401).json([{
+          msg: 'dang ky that bai'
+        }]);
+      }
+    });
 
   });
 
   // login
-  router.post('/login', function (req, res, next) {
+  router.post('/auth/login', function (req, res, next) {
 
     req.check('username', 'username khong duoc de trong').notEmpty();
     req.check('password', 'password khong duoc de trong').notEmpty();
@@ -48,17 +65,56 @@ module.exports = function (router) {
 
   }, function (req, res) {
 
-    User.findOne({username: req.body.username, password: req.body.password}, function (err, user) {
-
+    User.findOne({
+      username: req.body.username
+    }, function (err, user) {
       if (user) {
-        let token = jwt.sign({user: user}, process.env.JWT_SECRET_KEY, {expiresIn: 60*60});
-        return res.status(200).json({token: token});
+        if (bcrypt.compareSync(req.body.password, user.password)) {
+          let token = jwt.sign({
+            user: user
+          }, process.env.JWT_SECRET_KEY, {
+            expiresIn: 60 * 60
+          });
+          return res.status(200).json({
+            token: token,
+            user: user
+          });
+        } else {
+          return res.status(401).json([{
+            msg: 'dang nhap that bai'
+          }]);
+        }
       } else {
-        return res.status(401).json([{msg: 'dang nhap that bai'}]);
+        return res.status(401).json([{
+          msg: 'username khong ton tai'
+        }]);
       }
-
-    });    
+    });
 
   });
-  
+
+  // set user admin
+  router.get('/auth/set-admin', function (req, res) {
+
+    User.remove(function (err) {
+      if (!err) {
+        let user = new User();
+        user.username = 'admin';
+        user.password = bcrypt.hashSync('1', bcrypt.genSaltSync(10));
+        user.role = 1;
+        user.save(function (err, user) {
+
+          if (user) return res.status(200).json({
+            message: 'set user admin success'
+          });
+          return res.status(500).json({
+            message: 'set user admin error'
+          });
+
+        });
+      }
+    });
+
+  });
+
 }
--- models/User.js
+++ models/User.js
@@ -3,13 +3,14 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 const Schema = {
-  user_name: String,
+  username: String,
   email:String,
   password: String,
   state:Number,
   provider_id:Number,
   provider_name:String,
   code:String,
+  role: Number,
   profile: {
   	full_name:String,
   	job:String,
