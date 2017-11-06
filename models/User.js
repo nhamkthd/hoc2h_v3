c82cd9629c348921a5308118e3be6203045e04bd
huubv <huubv@kaopiz.com>
2017-11-06 10:55:04 +0700

    fix jwt

--- models/User.js
+++ models/User.js
@@ -1,28 +1,43 @@
-
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
+const Schema = mongoose.Schema;
 
-const Schema = {
+const User = new Schema({
   username: String,
-  email:String,
+  email: String,
   password: String,
-  state:Number,
-  provider_id:Number,
-  provider_name:String,
-  code:String,
+  state: Number,
+  provider_id: Number,
+  provider_name: String,
+  code: String,
   role: Number,
   profile: {
-  	full_name:String,
-  	job:String,
-  	local:String,
-  	phone:Number,
-  	birthday:Date,
-  	avatar:String,
-  	coins:Number,
-  	reputations:Number,
-  	introduction:String
+    full_name: String,
+    job: String,
+    local: String,
+    phone: Number,
+    birthday: Date,
+    avatar: String,
+    coins: Number,
+    reputations: Number,
+    introduction: String
   }
 
+});
+
+User.methods.signJwt = function (id) {
+  return jwt.sign({
+    user: id
+  }, process.env.JWT_SECRET_KEY);
 }
 
-module.exports = mongoose.model('User', Schema);
+User.methods.hashSync = function (password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+}
+
+User.methods.compareSync = function (password) {
+  return bcrypt.compareSync(password, this.password);
+}
+
+module.exports = mongoose.model('User', User);
