5abc490832459a429efa9a0e65fa62b35c06aa39
huubv <huubv@kaopiz.com>
2017-11-07 15:54:51 +0700

    update auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -67,23 +67,56 @@ module.exports = function (router) {
 
   });
 
+  // admin login
+  router.post('/auth/admin/login', function (req, res, next) {
+
+    req.check('username', 'username khong duoc phep de trong').notEmpty();
+    req.check('password', 'password khong duoc phep de trong').notEmpty();
+
+    let errors = req.validationErrors();
+    if (errors) return res.status(422).json(errors);
+    return next();
+
+  }, function (req, res) {
+
+    User.findOne({username: req.body.username}, function (err, user) {
+
+      if (err) return res.status(500).json(err);
+      if (user) {
+
+        if (user.role != 1 || ! user.compareSync(req.body.password)) return res.status(401).json('dang nhap that bai');
+        let token = user.signJwt(user.id);
+        return res.status(200).json({token: token, user: user});
+
+      }
+
+      return res.status(401).json('dang nhap that bai');
+
+    });
+
+  });
+
   // set user admin
   router.get('/auth/set-admin', function (req, res) {
 
-    User.remove(function (err) {
+    User.findOne({username: 'admin'}, function (err, user) {
 
-      if (err) return res.status(500).json(err);
+      if (! user) {
 
-      let user = new User();
-      user.username = 'admin';
-      user.password = user.hashSync('1');
-      user.role = 1;
-      user.save(function (err, user) {
+        let user = new User();
+        user.username = 'admin';
+        user.password = user.hashSync('1');
+        user.role = 1;
+        user.save(function (err, user) {
 
-        if (user) return res.status(200).json('set user admin success');
-        return res.status(401).json('set user admin error');
+          if (user) return res.status(200).json('set user admin success');
+          return res.status(401).json('set user admin error');
 
-      });
+        });
+
+      }
+
+      return res.status(401).json('user admin da ton tai');
 
     });
 
