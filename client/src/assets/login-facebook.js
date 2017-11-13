03fa91f6f75b145bc17f2f9c521ec1d7412ca42c
huubv <huubv@kaopiz.com>
2017-11-13 18:38:53 +0700

    fix login with facebook

--- client/src/assets/login-facebook.js
+++ client/src/assets/login-facebook.js
@@ -1,22 +1,20 @@
-var initFacebook = function (appId) {
-  window.fbAsyncInit = function () {
-    FB.init({
-      appId: appId,
-      autoLogAppEvents: true,
-      xfbml: true,
-      version: 'v2.11'
-    });
-    FB.AppEvents.logPageView();
-  };
+window.fbAsyncInit = function () {
+  FB.init({
+    appId: 1755570171410896,
+    autoLogAppEvents: true,
+    xfbml: true,
+    version: 'v2.11'
+  });
+  FB.AppEvents.logPageView();
+};
 
-  (function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {
-      return;
-    }
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-}
+(function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {
+    return;
+  }
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
